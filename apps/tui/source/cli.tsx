9c269d67c4b4172898cb305df165bcf4170c96c1
DevDesignAmitesh <amiteshsingh252@gmail.com>
2026-06-14 19:26:45 +0530

    idk

--- apps/tui/source/cli.tsx
+++ apps/tui/source/cli.tsx
@@ -1,30 +1,31 @@
+#!/usr/bin/env node
 import React from 'react';
 import { render } from 'ink';
-import meow from 'meow';
+// import meow from 'meow';
 import App from './app.js';
 import { WebSocketContextProvider } from './context/ws.js';
 
-const cli = meow(
-	`
-	Usage
-	  $ 01
+// const cli = meow(
+// 	`
+// 	Usage
+// 	  $ 01
 
-	Options
-		--name  Your name
+// 	Options
+// 		--name  Your name
 
-	Examples
-	  $ 01 --name=Jane
-	  Hello, Jane
-`,
-	{
-		importMeta: import.meta,
-		flags: {
-			name: {
-				type: 'string',
-			},
-		},
-	},
-);
+// 	Examples
+// 	  $ 01 --name=Jane
+// 	  Hello, Jane
+// `,
+// 	{
+// 		importMeta: import.meta,
+// 		flags: {
+// 			name: {
+// 				type: 'string',
+// 			},
+// 		},
+// 	},
+// );
 
 render((
 	<WebSocketContextProvider>
